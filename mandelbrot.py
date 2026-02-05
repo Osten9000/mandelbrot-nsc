f3b382b6705880ff4bb8b83d3cbfbd0114648056
philly cheese <phillycheese@h001476.wifi.general.client.rdns.aau.dk>
2026-02-05 15:03:02 +0100

    bruh

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,1 +1,1 @@
-"hej"
+"""Mandelbrot Set GeneratorAuthor: [Your Name]Course: Numerical Scientific Computing 2026 """def f(x): """Example function.Parameters ---------- x : floatInput valueReturns ------- floatOutput value """# TODO: Implement the algorithmpass
