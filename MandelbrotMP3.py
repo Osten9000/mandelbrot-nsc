0d381cea7de85f86dd1c0190638b143d1dd01b88
philly cheese <phillycheese@phillys-MacBook-Air.local>
2026-04-23 11:46:29 +0200

    fixed Mandelbrot

--- MandelbrotMP3.py
+++ MandelbrotMP3.py
@@ -1,20 +1,14 @@
 import numpy as np
 
-def mandelbrot_set(x_min, x_max, y_min, y_max, width, height):
+def mandelbrot_set(x_min, x_max, y_min, y_max, width, height, max_iter=100): 
     x = np.linspace(x_min, x_max, width)
     y = np.linspace(y_min, y_max, height)
-
     X, Y = np.meshgrid(x, y)
-
-    C= X + 1j*Y
-    
-    Z = np.zeros_like(C)  
-    M = np.zeros((height, width), dtype=int)  
-    
-    for n in range(max_iter):
+    C = X + 1j * Y
+    Z = np.zeros_like(C)
+    M = np.zeros((height, width), dtype=int)
+    for n in range(max_iter):        
         mask = np.abs(Z) <= 2
-        
-        Z[mask] = Z[mask]**2 + C[mask]
-        
+        Z[mask] = Z[mask] ** 2 + C[mask]
         M[mask] += 1
     return M
